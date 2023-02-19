5aa074a3015dfd7f9b142ea747cad6d5cbe5f2a1
rgf-rgharat <125173538+rgf-rgharat@users.noreply.github.com>
2023-02-19 11:04:48 +0530

    Project Started

---
+++ create-account/src/test/java/com/registration/app/createaccount/CreateAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registration.app.createaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
