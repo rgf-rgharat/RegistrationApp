511574f153c37c067ddd7ccb33ec91ae01010081
Rahul Gharat <Rahul.Gharat@harbingergroup.com>
2023-03-27 02:00:47 +0530

    modified signup page

---
+++ create-account/src/main/resources/templates/scr.js
@@ -0,0 +1,27 @@
+function checkpassword(){
+
+    let password= document.getElementById("passwordh").value;
+    let confirmpassword= document.getElementById("confirmpasswordh").value;
+    let errmsg=document.getElementById("errormsg").value;
+
+    if(password.length !=0){
+        if(password==confirmpassword){
+            errmsg.textContent="Password Match";
+
+        }else {
+            errmsg.textContent="password dont match";
+        }
+    }
+}
+
+
+// document.querySelector('btn btn-success').onclick=function(){
+
+//     var password=document.querySelector('.passwordh').value,
+//         confirmpassword=document.querySelector('confirmpasswordh').value;
+
+//         if(password==confirmpassword){
+//             alert("matched");
+//         }
+
+// }
